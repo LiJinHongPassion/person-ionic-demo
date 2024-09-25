374b85495541cbc0f9bfd8a79e1485ef0f8fef45
lijinhong <lijinhong@icloud.com>
2024-09-25 19:05:12 +0800

    管理页面导航增加

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  list = [
+    {
+      color: 'danger',
+      iconName: 'person-outline',
+      labelId: 'person.mgmt',
+      routerLink: '/person'
+    },
+    {
+      color: 'danger',
+      iconName: 'people-outline',
+      labelId: 'group.mgmt',
+      routerLink: '/group'
+    },
+    {
+      color: 'danger',
+      iconName: 'create-outline',
+      labelId: 'event.mgmt',
+      routerLink: '/event'
+    },
+    {
+      color: 'danger',
+      iconName: 'globe-outline',
+      labelId: 'relation.mgmt',
+      routerLink: '/relation'
+    },
+    {
+      color: 'danger',
+      iconName: 'server-outline',
+      labelId: 'data.mgmt',
+      routerLink: '/data'
+    },
+  ]
   constructor() {}
 
 }
