c58f102c7b16f2476516dc204ad806573dd499f3
lijinhong <lijinhong@icloud.com>
2024-10-09 20:18:01 +0800

    事件页面创建

--- src/app/pages/event/event-routing.module.ts
+++ src/app/pages/event/event-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; 
 import { EventListComponent } from './event-list/event-list.component';
+import { EventInfoComponent } from './event-info/event-info.component';
+import { EventEditComponent } from './event-edit/event-edit.component';
 
 const routes: Routes = [
   {
     path: '',
     component: EventListComponent
+  }, 
+  {
+    path: 'edit',
+    component: EventEditComponent
   },
+  {
+    path: 'modify/:id',
+    component: EventEditComponent
+  },
+  {
+    path: 'detail/:id',
+    component: EventInfoComponent
+  }
 
 ];
 
--- src/app/pages/event/event.module.ts
+++ src/app/pages/event/event.module.ts
@@ -2,14 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventRoutingModule } from './event-routing.module';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { AgeModule } from 'src/app/component/pipe/age/age.module';
+import { WorthModule } from 'src/app/component/pipe/worth/worth.module';
+import { TagModule } from 'src/app/component/tag/tag.module';
+import { TimelineEntryModule } from 'src/app/component/timeline-entry/timeline-entry.module';
+import { EventEditComponent } from './event-edit/event-edit.component';
+import { EventInfoComponent } from './event-info/event-info.component';
 
 
 
 @NgModule({
-  declarations: [EventListComponent],
+  declarations: [EventListComponent, EventEditComponent,EventInfoComponent],
   imports: [
     CommonModule,
-    EventRoutingModule
+    EventRoutingModule,
+    FormsModule,
+    IonicModule,
+    // 引入国际化模块
+    TranslateModule, 
+    TagModule,
+    WorthModule,
+    AgeModule,
+    TimelineEntryModule
   ]
 })
 export class EventModule { }
